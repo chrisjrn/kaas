7a83c09551fe8bd9ea89b413b177ad49719e75af
Christopher Neugebauer <chrisjrn@gmail.com>
2013-11-06 20:49:17 +1100

    More methods in slideshow.py

--- slideshow.py
+++ slideshow.py
@@ -13,6 +13,13 @@ class Slideshow(object):
 		self.__kpfdir__ = kpfdir
 		self.__kpf__ = kpfutil.Kpf(os.path.join(kpfdir, "kpf.json"))
 
+		# Variables for guessing where keynote is at the moment. Lol.
+		self.current_build = 0 # Builds are 0-indexed
+		self.current_slide = 1 # Slides are 1-indexed
+		self.certain = False   # Are we certain that current_build is correct?
+
+	''' Lifecycle methods for slideshow '''
+
 	def prepare(self):
 		''' Assembles all of the builds of the slides from the KPF transition
 		information; at the conclusion of this, we'll have a series of slide builds
@@ -21,10 +28,36 @@ class Slideshow(object):
 		os.mkdir(builds_dir)
 		self.__kpf__.assemble_slides(builds_dir)
 		self.prepared = True
+		self.builds_dir = builds_dir
 
 	def obliterate(self):
+		''' Deletes the exported slideshow; most of this object will be invalid
+		from now on. '''
 		subprocess.call(["rm", "-rf", self.__kpfdir__])
 
+	''' Methods for getting data to the consumer '''
+
+	def notes(self, slide):
+		''' Gets presenter notes for the given slide '''
+		return self.__kpf__["notes"]["slide-%d" % slide]
+
+	def build_preview(self, event):
+		''' Returns the filename for the build preview for the
+		given event. Must be called AFTER self.prepare() -- this creates
+		the build previews. '''
+
+		assert(self.prepared)
+		
+		return os.path.join(self.builds_dir, "build_%d.jpg" % event)
+
+	def find_next_still(self, event):
+		''' Finds the next event that will be displayed after the given event '''
+
+		i = event + 1
+		timelines = self.__kpf__["eventTimelines"]
+		while timelines[i]["automaticPlay"] != 0 and i <= len(timelines):
+			i += 1
+		return 1
 
 
 def generate():
@@ -52,6 +85,11 @@ if __name__ == "__main__":
 
 	print >> sys.stderr, "Preparing builds: "
 	slideshow.prepare()
+
+	k = keynote_script()
+
+	# Go to given slide; show filename for still for where screen will stop.
+
 
 	raw_input("Press enter to proceed")
 
