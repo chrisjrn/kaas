09966b4439e2e2d501d08b2ab0001461160b705d
Christopher Neugebauer <chrisjrn@gmail.com>
2013-11-06 19:20:03 +1100

    OO-ifies kpfutil.py; exposes assemble_slides() as a method.

--- kpfutil.py
+++ kpfutil.py
@@ -5,60 +5,44 @@ import sys
 import os
 from PIL import Image
 
-kpffile= sys.argv[1]
 
-kpf = json.load(open(kpffile))
-kpfdir = os.path.dirname(kpffile)
+class Kpf(object):
 
+	def __init__(self, filename):
+		self.kpfdir = os.path.dirname(filename) # The directory where the textures can be found
+		self.kpf = json.load(open(filename))
 
-def print_slide_assembly():
-	for event_index, event in enumerate(kpf["eventTimelines"]):
-
-		for state_index, state in enumerate(event["eventInitialStates"]):
-
-			texture = state["texture"]
-			print event_index, state_index, kpf["textures"][texture]["url"]
-
-def first_initial_transforms():
-	for event_index, event in enumerate(kpf["eventTimelines"]):
-
-		state = event["eventInitialStates"][0]
-
-		transform = state["affineTransform"]
-		print transform
+	def assemble_slides(self, output_directory):
+		assemble_slides(self, output_directory)
 
 
+def assemble_slides(kpf, output_directory):
 
-def assemble_slides():
-
-	for event_index, event in enumerate(kpf["eventTimelines"]):
-		assemble_slide(event_index, event)
+	for event_index, event in enumerate(kpf.kpf["eventTimelines"]):
+		assemble_slide(kpf, output_directory, event_index, event)
 
 
-def assemble_slide(build_index, event):
-	textures = kpf["textures"]
+def assemble_slide(kpf, output_directory, build_index, event):
 
-	im = None
+	im = Image.new("RGBA", (kpf.kpf["slideWidth"], kpf.kpf["slideHeight"]))
 
 	for state_index, state in enumerate(event["eventInitialStates"]):
 		if state["hidden"] != 0:
 			continue
-		texture = textures[state["texture"]]["url"]
-		if im == None:
-			#print "boop"
-			im = Image.open(os.path.join(kpfdir,texture))
-		else:
-			add_texture(im, texture, state["affineTransform"])
-			pass
+		add_texture(kpf, im, state)
 
-	fn = "build_%000d.jpg" % (build_index + 1)
+	fn = os.path.join(output_directory, "build_%00000d.jpg" % (build_index))
 	im.save(fn)
 
 
 
-def add_texture(image, texture, transform):
+def add_texture(kpf, image, state):
+	textures = kpf.kpf["textures"]
+	transform = state["affineTransform"]
+	texture_file = textures[state["texture"]]["url"]
 	tx, n0, n1, ty, sx, sy = transform
-	tex = Image.open(os.path.join(kpfdir,texture))
+	tex = Image.open(os.path.join(kpf.kpfdir, texture_file))
+	# TODO: support opacity
 	if (tx != 1 or ty != 1):
 		tex = tex.resize((tx, ty))
 	if tex.mode == "RGBA":
@@ -67,9 +51,14 @@ def add_texture(image, texture, transform):
 		image.paste(tex, (sx, sy))
 
 
-
 #first_initial_transforms()
-assemble_slides()
+def main():
+	kpffile = sys.argv[1]
+	kpf = Kpf(kpffile)
+	kpf.assemble_slides(sys.argv[2])
+
+if __name__ == "__main__":
+	main()
 
 
 
