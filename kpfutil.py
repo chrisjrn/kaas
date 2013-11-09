fdc487d51dad694fd1cca26e583cd930f76e2830
Christopher Neugebauer <chrisjrn@gmail.com>
2013-11-09 13:05:13 +1100

    Replaces NSImage drawing commands with NSBitmapRep drawing commands. Thanks @kongtomorrow

--- kpfutil.py
+++ kpfutil.py
@@ -6,7 +6,10 @@ import os
 
 from AppKit import NSBitmapImageRep
 from AppKit import NSCompositeSourceOver
+from AppKit import NSDeviceRGBColorSpace
+from AppKit import NSGraphicsContext
 from AppKit import NSImage
+from AppKit import NSImageRep
 from AppKit import NSJPEGFileType
 from AppKit import NSMakeRect
 from AppKit import NSPoint
@@ -38,7 +41,8 @@ def assemble_slides(kpf, output_directory):
 
 def assemble_slide(kpf, output_directory, build_index, event):
 
-	im = NSImage.alloc().initWithSize_(NSSize(kpf.kpf["slideWidth"], kpf.kpf["slideHeight"]))
+	init = NSBitmapImageRep.alloc().initWithBitmapDataPlanes_pixelsWide_pixelsHigh_bitsPerSample_samplesPerPixel_hasAlpha_isPlanar_colorSpaceName_bytesPerRow_bitsPerPixel_
+	im = init(None, kpf.kpf["slideWidth"], kpf.kpf["slideHeight"], 8, 4, True, False, NSDeviceRGBColorSpace, 0, 0)
 
 	for state_index, state in enumerate(event["eventInitialStates"]):
 		if state["hidden"] != 0:
@@ -47,12 +51,8 @@ def assemble_slide(kpf, output_directory, build_index, event):
 
 	fn = os.path.join(output_directory, "build_%00000d.jpg" % (build_index))
 
-	im.lockFocus()
-	sz = im.size()
-	imrep = NSBitmapImageRep.alloc().initWithFocusedViewRect_(NSMakeRect(0,0,sz.width, sz.height))
-	imjpeg = imrep.representationUsingType_properties_(NSJPEGFileType, None)
+	imjpeg = im.representationUsingType_properties_(NSJPEGFileType, None)
 	imjpeg.writeToFile_atomically_(fn, False)
-	im.unlockFocus()
 
 
 def add_texture(kpf, image, state):
@@ -61,7 +61,6 @@ def add_texture(kpf, image, state):
 	texture_file = textures[state["texture"]]["url"]
 	sx, n0, n1, sy, tx, ty = transform
 	
-	#tex = Image.open(os.path.join(kpf.kpfdir, texture_file))
 	tex = NSImage.alloc().initWithContentsOfFile_(os.path.join(kpf.kpfdir, texture_file))
 
 	# TODO: support opacity
@@ -69,13 +68,13 @@ def add_texture(kpf, image, state):
 		#tex = tex.resize((sx, sy))
 		tex.setSize_(NSSize(sx, sy))
 
-	image.lockFocus()
+	context = NSGraphicsContext.graphicsContextWithBitmapImageRep_(image)
+	context.setCompositingOperation_(NSCompositeSourceOver)
+	NSGraphicsContext.setCurrentContext_(context)
 
 	# Cocoa uses inverted Y-axis...
 	ty_ = image.size().height - tex.size().height - ty
 	tex.drawAtPoint_fromRect_operation_fraction_(NSPoint(tx, ty_), NSZeroRect, NSCompositeSourceOver, 1.0)
-
-	image.unlockFocus()
 
 
 #first_initial_transforms()
