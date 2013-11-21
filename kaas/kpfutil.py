46001b09f74cc05a6bb180079c88c291e4c4cc01
Christopher Neugebauer <chrisjrn@gmail.com>
2013-11-21 23:12:47 +1100

    First pass at K6 display; getting there; not quite there yet.

--- kaas/kpfutil.py
+++ kaas/kpfutil.py
@@ -135,6 +135,67 @@ class KpfV5(Kpf):
         return TextureV5(self, name)
 
 
+class KpfV6(Kpf):
+
+    def __init__(self, kpfdir):
+        self.kpfdir = kpfdir # The directory where the textures can be found
+        filename = os.path.join(kpfdir, "header.json")
+
+        kpfjson  = open(filename).read()
+        self.kpf = json.loads(kpfjson)
+
+        self.__width__ = self.kpf["slideWidth"]
+        self.__height__ = self.kpf["slideHeight"]
+
+        h = hashlib.sha256()
+        h.update(kpfjson)
+        self.__hash__ = h.hexdigest()
+
+        slide_file_names = self.kpf["slideList"]
+        self.__raw_slides__ = {}
+        self.__builds__ = []
+        self.__textures__ = {}
+        for _i, name in enumerate(slide_file_names):
+            i = _i + 1
+            slide_file = os.path.join(kpfdir, name, name+".json")
+            raw_slide = json.load(open(slide_file))
+            self.__raw_slides__[i] = raw_slide
+            self.__process_slide__(i, name, raw_slide)
+
+    def __process_slide__(self, index, name, raw_slide):
+        for filename, asset in raw_slide["assets"].items():
+            self.__textures__[filename] = TextureV6(self, name, asset["url"])
+
+        for event in raw_slide["events"]:
+            build = BuildV6(self, index, event)
+            self.__builds__.append(build)
+
+
+    def build(self, index):
+        return self.__builds__[index]
+
+    def build_count(self):
+        return len(self.__builds__)
+
+    def build_is_autoplay(self, build):
+        ''' Returns true is the build is an autoplay build '''
+        return self.__builds__[build].build_raw["automaticPlay"]
+
+    def navigator_events(self):
+        ''' Returns a sparse array (dictionary) of the first
+        builds for every slide '''
+        return {}
+
+    def notes(self, slide):
+        ''' Returns the notes for the given slide '''
+        return ""
+
+    def slide_count(self):
+        return self.kpf["slideCount"] 
+
+    def texture(self, name):
+        return self.__textures__[name]
+
 class Build(object):
 
     def state(self, index):
@@ -182,6 +243,18 @@ class BuildV5(Build):
     def state_count(self):
         return len(self.initial_states_raw)
 
+class BuildV6(Build):
+
+    def __init__(self, kpf_v6, slide_index, build_raw):
+        self.build_raw = build_raw
+        self.kpf_v6 = kpf_v6
+
+    def state(self, index):
+        return EventStateV6(self.kpf_v6, self.build_raw["baseLayer"]["layers"][index])
+
+    def state_count(self):
+        return len(self.build_raw["baseLayer"]["layers"])
+
 
 class EventStateV5(EventState):
 
@@ -200,6 +273,34 @@ class EventStateV5(EventState):
         ''' Returns the affine transform to be applied to the texture '''
         return self.event_state_raw["affineTransform"]
 
+class EventStateV6(EventState):
+
+    def __init__(self, kpf_v6, event_state_raw):
+        self.kpf_v6 = kpf_v6
+        self.event_state_raw = event_state_raw
+
+
+    def is_hidden(self):
+        ''' Is this event state hidden? '''
+        return self.event_state_raw["initialState"]["hidden"] != 0
+
+    def texture(self):
+        ''' Returns the texture that this event state applies to '''
+        texture = self.find_texture(self.event_state_raw) #["layers"][0]["texture"]
+        return self.kpf_v6.texture(texture)
+
+    def transform(self):
+        ''' Returns the affine transform to be applied to the texture '''
+        return self.event_state_raw["layers"][0]["initialState"]["affineTransform"]
+
+    def find_texture(self, dc):
+        if "texture" in dc:
+            return dc["texture"]
+        if "layers" in dc:
+            return self.find_texture(dc["layers"][0])
+
+
+
 
 class TextureV5(Texture):
 
@@ -211,6 +312,16 @@ class TextureV5(Texture):
         textures = self.kpf_v5.kpf["textures"]
         texture_file = textures[self.texture]["url"]
         return os.path.join(self.kpf_v5.kpfdir, texture_file)
+
+class TextureV6(Texture):
+
+    def __init__(self, kpf_v6, slide_path, asset):
+        self.kpf_v6 = kpf_v6
+        self.slide_path = slide_path 
+        self.asset = asset
+
+    def path(self):
+        return os.path.join(self.kpf_v6.kpfdir, self.slide_path, self.asset)
 
 
 def assemble_slides(kpf, output_directory):
@@ -227,7 +338,9 @@ def assemble_slide(kpf, output_directory, build_index, event):
     for state_index, state in enumerate(event.states()):
         if state.is_hidden() != 0:
             continue
+        print state.texture().path()
         add_texture(kpf, im, state)
+    print
 
     fn = os.path.join(output_directory, "build_%00000d.jpg" % (build_index))
 
@@ -259,8 +372,8 @@ def add_texture(kpf, image, state):
 #first_initial_transforms()
 def main():
     kpffile = sys.argv[1]
-    kpf = Kpf(kpffile)
-    kpf.assemble_slides(sys.argv[2])
+    kpf = KpfV6(kpffile)
+    kpf.assemble_slides(sys.argv[1] + "/builds")
 
 if __name__ == "__main__":
     main()
